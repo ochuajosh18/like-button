0b34123a8290a94f6cdfbd05a823f21ed6b43053
ochuajosh18 <abayajoshua01@gmail.com>
2022-01-12 22:21:41 +0800

    modified like.js

--- like.js
+++ like.js
@@ -1,9 +1,19 @@
 //javascript
+
+
+
  let count = 0;
 let like = document.querySelector("#like_btn");
 let like_count = document.querySelector("#like_counter");
-like.addEventListener("click",  function(){
-    count += 1;
+like.addEventListener("click",  function (){
+    count +=1;
     like_count.innerHTML = count;
-    
 });
+   like.addEventListener("click", function(){
+    if(count%2==0){
+        console.log("The number " +count+ " is Even");
+    } else{
+        console.log("The number "+count+ " is Odd");
+    }
+   });
+
